ed20612cbb48c465fc51f8612a2c88bcd24eb61f
ChelakaLiyanageGNeuro <chelaka.gneuro@gmail.com>
2026-04-22 15:42:08 +0530

    Update Home component to include Statistics and improve background styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero/hero";
 import { Navbar } from "@/components/navbar/navbar";
+import { Statistics } from "@/components/statistics/statistics";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-zinc-950">
+    <main className="min-h-screen bg-background text-foreground">
       <Navbar />
       <Hero />
+      <Statistics />
     </main>
   );
 }
